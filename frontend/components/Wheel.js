b4ef206ab06bcee2210508d443440a491c79bbbb
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-06-22 14:49:52 -0600

    it begins

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function Wheel(props) {
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
+        <div
+          className={`cog ${props.wheel === 0 ? "active" : ""}`}
+          style={{ "--i": 0 }}
+          id="0"
+        >
+          {props.wheel === 0 ? "B" : ""}
+        </div>
         <div className="cog" style={{ "--i": 1 }}></div>
         <div className="cog" style={{ "--i": 2 }}></div>
         <div className="cog" style={{ "--i": 3 }}></div>
         <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className="cog" style={{ "--i": 5 }}></div>
+        {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
+        <button id="counterClockwiseBtn">Counter clockwise</button>
         <button id="clockwiseBtn">Clockwise</button>
       </div>
     </div>
-  )
+  );
 }
--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,17 +1,36 @@
+import {
+  MOVE_CLOCKWISE,
+  MOVE_COUNTERCLOCKWISE,
+  SET_QUIZ_INTO_STATE,
+  SET_SELECTED_ANSWER,
+  SET_INFO_MESSAGE,
+  INPUT_CHANGE,
+  RESET_FORM,
+} from "./action-types";
+import axios from "axios";
+
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+export function moveClockwise() {
+  return {
+    type: MOVE_CLOCKWISE,
+  };
+}
 
-export function moveCounterClockwise() { }
+export function moveCounterClockwise() {
+  return {
+    type: MOVE_COUNTERCLOCKWISE,
+  };
+}
 
-export function selectAnswer() { }
+export function selectAnswer() {}
 
-export function setMessage() { }
+export function setMessage() {}
 
-export function setQuiz() { }
+export function setQuiz() {}
 
-export function inputChange() { }
+export function inputChange() {}
 
-export function resetForm() { }
+export function resetForm() {}
 
 // ❗ Async action creators
 export function fetchQuiz() {
@@ -19,7 +38,7 @@ export function fetchQuiz() {
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
-  }
+  };
 }
 export function postAnswer() {
   return function (dispatch) {
@@ -27,13 +46,13 @@ export function postAnswer() {
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
-  }
+  };
 }
 export function postQuiz() {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
-  }
+  };
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
